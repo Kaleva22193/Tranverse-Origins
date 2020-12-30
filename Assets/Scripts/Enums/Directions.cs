eb6e47ab1084fe70ca2ceca79c5781d9a4aec669
Kaleva <bobheath2005@gmail.com>
2020-12-30 17:19:32 -0500

    Inital commit

---
+++ Assets/Scripts/Enums/Directions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Rooms
+{
+    public enum Directions {North, South, East, West, NorthEast, SouthEast, NorthWest, SouthWest}
+
+}
